ee80242361cce294d800b4b1f01bd63169eb27c8
wangsongqing <xmwme@qq.com>
2022-06-21 15:57:00 +0800

    管理后台 - 用户列表

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -8,9 +8,7 @@ Route::group([
     'prefix'        => config('admin.route.prefix'),
     'namespace'     => config('admin.route.namespace'),
     'middleware'    => config('admin.route.middleware'),
-    'as'            => config('admin.route.prefix') . '.',
 ], function (Router $router) {
-
-    $router->get('/', 'HomeController@index')->name('home');
-
+    $router->get('/', 'HomeController@index');
+    $router->get('users', 'UsersController@index');
 });
--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Encore\Admin\Traits\DefaultDatetimeFormat;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -11,7 +12,8 @@ use Laravel\Sanctum\HasApiTokens;
 class User extends Authenticatable implements MustVerifyEmail
 {
     use HasApiTokens, HasFactory, Notifiable;
-
+    // 加上这个 Trait
+    use DefaultDatetimeFormat;
     /**
      * The attributes that are mass assignable.
      *
