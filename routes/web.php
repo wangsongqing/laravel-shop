8ad11095a5fdce96f6514b3ac37ecf9a920d9aec
wangsongqing <xmwme@qq.com>
2022-06-21 11:43:10 +0800

    收货地址列表

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,9 @@ Route::get('/', 'PagesController@root')->name('root');
 
 // 在之前的路由里加上一个 verify 参数
 Auth::routes(['verify' => true]);
+
+
+// auth中间件代表需要登录，verified中间件代表需要经过邮箱验证
+Route::group(['middleware' => ['auth', 'verified']], function() {
+    Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
+});
