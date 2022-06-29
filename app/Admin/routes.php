010c1f1730ff854faa0e2444370a99b617f7820c
wangsongqing <xmwme@qq.com>
2022-06-29 14:21:47 +0800

    管理后台 - 新增优惠券

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -21,4 +21,6 @@ Route::group([
     $router->post('orders/{order}/ship', 'OrdersController@ship')->name('orders.ship');
     $router->get('coupon_codes', 'CouponCodesController@index');
     $router->post('orders/{order}/refund', 'OrdersController@handleRefund')->name('orders.handle_refund');
+    $router->post('coupon_codes', 'CouponCodesController@store');
+    $router->get('coupon_codes/create', 'CouponCodesController@create');
 });
