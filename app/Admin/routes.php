555565ea370e99135af7c393c60b87b8320e6ab1
wangsongqing <xmwme@qq.com>
2022-06-29 14:26:33 +0800

    管理后台 - 修改、删除优惠券

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -23,4 +23,7 @@ Route::group([
     $router->post('orders/{order}/refund', 'OrdersController@handleRefund')->name('orders.handle_refund');
     $router->post('coupon_codes', 'CouponCodesController@store');
     $router->get('coupon_codes/create', 'CouponCodesController@create');
+    $router->get('coupon_codes/{id}/edit', 'CouponCodesController@edit');
+    $router->put('coupon_codes/{id}', 'CouponCodesController@update');
+    $router->delete('coupon_codes/{id}', 'CouponCodesController@destroy');
 });
