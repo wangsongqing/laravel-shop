314aea2b5e0c85071e29f1dd5a23c7020b73f7f6
wangsongqing <xmwme@qq.com>
2022-07-07 11:38:05 +0800

    订单模块调整

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -9,6 +9,13 @@ use Ramsey\Uuid\Uuid;
 class Order extends Model
 {
     use HasFactory;
+    const TYPE_NORMAL = 'normal';
+    const TYPE_CROWDFUNDING = 'crowdfunding';
+
+    public static $typeMap = [
+        self::TYPE_NORMAL => '普通商品订单',
+        self::TYPE_CROWDFUNDING => '众筹商品订单',
+    ];
 
     const REFUND_STATUS_PENDING = 'pending';
     const REFUND_STATUS_APPLIED = 'applied';
@@ -49,6 +56,7 @@ class Order extends Model
         'ship_status',
         'ship_data',
         'extra',
+        'type'
     ];
 
     protected $casts = [
