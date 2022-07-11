52f6d450b8d2482b8a1989b4be92a1e695fa25be
wangsongqing <xmwme@qq.com>
2022-07-11 10:05:32 +0800

    初始化分期付款模型

--- app/helpers.php
+++ app/helpers.php
@@ -36,8 +36,11 @@ function object_to_array($obj) {
 
     }
 
-
-
     return $arr;
+}
 
+// 默认的精度为小数点后两位
+function big_number($number, $scale = 2)
+{
+    return new \Moontoast\Math\BigNumber($number, $scale);
 }
