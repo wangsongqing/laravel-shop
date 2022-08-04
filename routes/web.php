22366175215bd74eaf947d88bc65aa2606d3189c
wangsongqing <xmwme@qq.com>
2022-08-04 15:54:09 +0800

    前台分期付款列表页

--- routes/web.php
+++ routes/web.php
@@ -50,6 +50,7 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
     Route::post('crowdfunding_orders', 'OrdersController@crowdfunding')->name('crowdfunding_orders.store');
 
     Route::post('payment/{order}/installment', 'PaymentController@payByInstallment')->name('payment.installment');
+    Route::get('installments', 'InstallmentsController@index')->name('installments.index');
 });
 
 Route::get('products/{product}', 'ProductsController@show')->name('products.show');
