bd1073a5b873d2415bbf038b762bfa9d19ef16f8
wangsongqing <xmwme@qq.com>
2022-08-05 13:41:32 +0800

    商品添加长标题字段

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -19,7 +19,8 @@ class Product extends Model
 
     protected $fillable = [
         'title', 'description', 'image', 'on_sale',
-        'rating', 'sold_count', 'review_count', 'price' , 'type'
+        'rating', 'sold_count', 'review_count', 'price' , 'type',
+        'long_title', // 添加 long_title 到 $fillable 属性中
     ];
 
     protected $casts = [
